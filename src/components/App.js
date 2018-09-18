d45d35daf2dfe1babaf530a760d3e38e57dc7954
Matheus Crepaldi <matheus.crepaldi@evolutionit.com.br>
2018-09-18 14:51:28 -0300

    rotas

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import SignInPage from "./SignIn";
 import PasswordForgetPage from "./PasswordForget";
 import HomePage from "./Home";
 import AccountPage from "./Account";
+import LoginPage from "../views/LoginPage/LoginPage";
 
 import * as routes from "../constants/routes";
 
@@ -29,6 +30,7 @@ const App = () => (
       />
       <Route exact path={routes.HOME} component={HomePage} />
       <Route exact path={routes.ACCOUNT} component={AccountPage} />
+      <Route exact path={routes.LOGIN} component={LoginPage} />
     </div>
   </Router>
 );
